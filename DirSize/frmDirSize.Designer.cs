611ac10ff061c4677e26db522e7ded15122ef340
nnyagolov <synchu@synchu.com>
2016-07-06 14:22:44 +0300

    Add project files.

---
+++ DirSize/frmDirSize.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace DirSize
+{
+    partial class frmDirSize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dgvFolders = new System.Windows.Forms.DataGridView();
+            this.colType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colModifiedDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pkId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFolders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(945, 509);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(845, 509);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // dgvFolders
+            // 
+            this.dgvFolders.AllowUserToAddRows = false;
+            this.dgvFolders.AllowUserToDeleteRows = false;
+            this.dgvFolders.AllowUserToOrderColumns = true;
+            this.dgvFolders.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvFolders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFolders.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colType,
+            this.colName,
+            this.colModifiedDate,
+            this.colSize,
+            this.pkId});
+            this.dgvFolders.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvFolders.Location = new System.Drawing.Point(12, 29);
+            this.dgvFolders.Name = "dgvFolders";
+            this.dgvFolders.Size = new System.Drawing.Size(1012, 455);
+            this.dgvFolders.TabIndex = 2;
+            this.dgvFolders.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFolders_CellDoubleClick);
+            this.dgvFolders.CellToolTipTextNeeded += new System.Windows.Forms.DataGridViewCellToolTipTextNeededEventHandler(this.dgvFolders_CellToolTipTextNeeded);
+            // 
+            // colType
+            // 
+            this.colType.DataPropertyName = "colType";
+            this.colType.HeaderText = "Type";
+            this.colType.Name = "colType";
+            // 
+            // colName
+            // 
+            this.colName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCellsExceptHeader;
+            this.colName.DataPropertyName = "colName";
+            this.colName.HeaderText = "Name";
+            this.colName.Name = "colName";
+            this.colName.Width = 5;
+            // 
+            // colModifiedDate
+            // 
+            this.colModifiedDate.DataPropertyName = "colModifiedDate";
+            this.colModifiedDate.HeaderText = "Modified Date";
+            this.colModifiedDate.Name = "colModifiedDate";
+            // 
+            // colSize
+            // 
+            this.colSize.DataPropertyName = "colSize";
+            this.colSize.HeaderText = "Size (KB)";
+            this.colSize.Name = "colSize";
+            // 
+            // pkId
+            // 
+            this.pkId.DataPropertyName = "pkId";
+            this.pkId.HeaderText = "pkId";
+            this.pkId.Name = "pkId";
+            this.pkId.Visible = false;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 509);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(101, 23);
+            this.btnSelectFolder.TabIndex = 3;
+            this.btnSelectFolder.Text = "Select folder...";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(138, 511);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(462, 514);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar1.Location = new System.Drawing.Point(492, 509);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(217, 23);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // frmDirSize
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 544);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.dgvFolders);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnClose);
+            this.Name = "frmDirSize";
+            this.Text = "Dir Size Listing Generator";
+            this.Load += new System.EventHandler(this.frmDirSize_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFolders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dgvFolders;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colModifiedDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pkId;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ DirSize/frmDirSize.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirSize
+{
+    public partial class frmDirSize : Form
+    {
+        private BindingSource dsFolders = new BindingSource();
+        DataTable dtFolders = null;
+        string selectedPath = "C:\\temp";
+        List<string> log = new List<string>();
+        bool bLog = false;
+
+
+        public frmDirSize()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDirSize_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = selectedPath;
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                selectedPath = fbd.SelectedPath;
+                textBox1.Text = selectedPath;
+                getDirs(selectedPath);
+                assignSources();
+                formatSize();
+            }
+        }
+
+
+        private void getDirs(string selectedFolder)
+        {
+            label1.Visible = false;
+            progressBar1.Visible = true;
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+
+            initDataTable();
+            ProcessDirectory(selectedFolder, 0, 0);
+           
+            formatDataViewGrid();
+       
+            progressBar1.Visible = false;
+            label1.Visible = true;
+            progressBar1.Step = 1;
+        }
+
+        private void initDataTable()
+        {
+
+            if (dtFolders != null)
+            {
+                dtFolders.Dispose();
+            }
+
+            dtFolders = new DataTable("Folders");
+            DataColumn pkId = dtFolders.Columns.Add("pkId", typeof(long));
+            dtFolders.Columns.Add("colType", typeof(string));
+            dtFolders.Columns.Add("colName", typeof(string));
+            dtFolders.Columns.Add("colSize", typeof(Int64));
+            dtFolders.Columns.Add("colModifiedDate", typeof(DateTime));
+
+            dtFolders.PrimaryKey = new DataColumn[] { pkId };
+        }
+
+        // Process all files in the directory passed in, recurse on any directories 
+        // that are found, and process the files they contain.
+        
+        private void ProcessDirectory(string targetDirectory, int firstLevelDir, Int64 pkId)
+        {
+            // Process the list of files found in the directory.
+            
+            
+            List<FileInfo> fiList = new List<FileInfo>();
+            DataRow drFolder = null;
+            long firstLevelPkId = 0;
+            string[] fileEntries = null;
+            bool someSkipped = false;
+
+
+            try
+            {
+              fileEntries = Directory.GetFiles(targetDirectory);
+            } catch (System.IO.PathTooLongException e)
+            {
+                log.Add(e.Message);
+            } catch (UnauthorizedAccessException e)
+            {
+                log.Add(e.Message);
+            }
+
+
+            if (firstLevelDir > -1)
+            {
+                try
+                {
+                    foreach (string fileName in fileEntries)
+                    {
+                        FileInfo fi = ProcessFile(fileName);
+                        fiList.Add(fi);
+                        if (firstLevelDir == 0)
+                        {
+                            drFolder = dtFolders.NewRow();
+                            drFolder["pkId"] = fi.GetHashCode();
+                            drFolder["colType"] = "File";
+                            drFolder["colName"] = fileName;
+                            drFolder["colModifiedDate"] = fi.LastWriteTime;
+                            drFolder["colSize"] = fi.Length;
+                            dtFolders.Rows.Add(drFolder);
+                        }
+                    }
+                } catch (Exception e)
+                {
+                    log.Add(e.Message);
+                }
+            }
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = null;
+
+            try
+            {
+               subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            } catch (System.IO.PathTooLongException e)
+            {
+                log.Add(e.Message);
+                someSkipped = true;
+                label1.Text = "Some folders might have been skipped, due to the path being too long!";                
+            } catch (UnauthorizedAccessException e)
+            {
+                log.Add(e.Message);
+                someSkipped = true;
+                label1.Text = "Some folders might have been skipped, due to no access permissions!";
+            }
+
+            
+
+            if (!someSkipped)
+            {
+                int progress = 0;
+                progressBar1.Value = 1;
+                progressBar1.Maximum = subdirectoryEntries.Length + 1;
+                progressBar1.Step = 4;
+
+                foreach (string subdirectory in subdirectoryEntries)
+                {
+
+                    if (firstLevelDir == 0)
+                    {
+                        drFolder = dtFolders.NewRow();
+                        drFolder["pkId"] = firstLevelPkId = new FileInfo(subdirectory).GetHashCode();
+                        drFolder["colType"] = "Folder";
+                        drFolder["colName"] = subdirectory;
+                        drFolder["colModifiedDate"] = new FileInfo(subdirectory).LastWriteTime;
+                        drFolder["colSize"] = 0;
+                        dtFolders.Rows.Add(drFolder);
+                    }
+                    else if (firstLevelDir > 0)
+                    {
+                        UpdateFolderSize(pkId, fiList);
+                    }
+
+                    ProcessDirectory(subdirectory, firstLevelDir + 1, firstLevelDir == 0 ? firstLevelPkId : pkId);
+
+                    if (progress % 4 == 0) progressBar1.PerformStep();
+                    
+                    progress++;
+                }
+
+
+                if (subdirectoryEntries.Length == 0 && firstLevelDir > 0)
+                {
+                    UpdateFolderSize(pkId, fiList);
+                }
+            }
+
+        }
+
+        private void UpdateFolderSize(long pkId, List<FileInfo> fiList)
+        {
+            DataRow[] foundRows = dtFolders.Select("pkId=" + pkId.ToString());
+            foundRows[0]["colSize"] = long.Parse(foundRows[0]["colSize"].ToString()) + getDirSize(fiList);
+            fiList.Clear();
+        }
+
+        private void formatDataViewGrid()
+        {
+            this.dgvFolders.Columns["colSize"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            this.dgvFolders.Columns["colModifiedDate"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+        }
+        private static FileInfo ProcessFile(string path)
+        {
+            return new FileInfo(path);
+        }
+
+        private Int64 getDirSize(List<FileInfo> fiList)
+        {
+            Int64 size = 0;
+            foreach (FileInfo fi in fiList)
+            {
+                size += fi.Length;
+                log.Add(fi.Name);
+            }
+            return size;
+        }
+
+        private void assignSources()
+        {
+            dsFolders.DataSource = dtFolders;
+            dgvFolders.DataSource = dsFolders;
+
+        }
+
+        private void formatSize()
+        {
+            var col = dtFolders.Columns["colSize"];
+            foreach (DataRow row in dtFolders.Rows)
+                row[col] = Convert.ToInt64(row[col])/1024 == 0? 1: Convert.ToInt64(row[col]) / 1024;
+        }
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrWhiteSpace(selectedPath)) {
+                getDirs(selectedPath);
+                assignSources();
+                formatSize();
+            } else
+            {
+                MessageBox.Show(this, "Please, select a folder first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (bLog)
+            {
+                log.Sort();
+                StreamWriter fs = new StreamWriter(selectedPath + "\\log.txt");
+                foreach (string s in log)
+                {
+                    fs.WriteLine(s);
+                }
+                fs.Flush();
+                fs.Close();
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox1.Text))
+            {
+                selectedPath = textBox1.Text;
+                label1.Text = "Correct selection";
+            } else
+            {
+                label1.Text = "Please, type or select an existing folder!";
+            }
+        }
+
+        private void dgvFolders_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > 0 && e.ColumnIndex > 0)
+            {
+                if (Directory.Exists(dgvFolders.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()))
+                {
+                    Process.Start(dgvFolders.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString());
+                }
+            }
+        }
+
+        private void dgvFolders_CellToolTipTextNeeded(object sender, DataGridViewCellToolTipTextNeededEventArgs e)
+        {
+
+            if (e.RowIndex > 0 && e.ColumnIndex > 0)
+            {
+                if (Directory.Exists(dgvFolders.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()))
+                {
+                    e.ToolTipText = "Double click to open this folder in Windows Explorer!";
+                }
+            }
+        }
+    }
+}
